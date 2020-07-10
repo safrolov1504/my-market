7691e5df78b34c16908139933759ed53a65ef2b7
safrolov1504 <serega7630190>
2020-07-10 15:06:21 +0300

    Initial commit

---
+++ src/test/java/com/geekbrains/my/market/mymarket/MyMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekbrains.my.market.mymarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
